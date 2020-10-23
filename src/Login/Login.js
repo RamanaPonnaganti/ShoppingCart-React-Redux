c9485c5dbd2593967e36d3765d949cbac653e3da
RamanaPonnaganti <73310486+RamanaPonnaganti@users.noreply.github.com>
2020-10-23 19:08:15 +0800

    Update Login.js

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -35,20 +35,34 @@ class Login extends React.Component {
         return (
             <div style={{height: '500px'}}>
                 <div className="login-form">
-                    <div style={{ justifyContent: 'center', textAlign: 'center', background: '#f7f4f4', height: 'auto', padding: '30px' }}>
-                        <div style={{ float: 'left', color: '#1d0707', fontWeight:'bold', marginBottom:"12px" }}>LOGIN</div>
-                        <div>
-                            <input type="text" value={this.state.userName} placeholder="Enter Username as admin" name="uname" onChange={this.changeuserName.bind(this)} required></input>
-                        </div>
-                        <div>
-                            <input type="password" value={this.state.password} placeholder="Enter Password as admin" name="psw" onChange={this.changePassword.bind(this)}></input>
-                        </div>
-                        <input type="submit" name="submit" value="Continue" onClick={this.login}></input>
-                    </div>
+				<div style={{ justifyContent: 'center', textAlign: 'center', background: '#f7f4f4', height: 'auto', padding: '30px' }}>
+				{
+					this.props.reduxState.isLoginSuccess ? <React.Fragment>
+						<div style={{color:'#1b1212'}}>WELCOME ADMIN!!!</div>
+						<div onClick={()=>{ this.props.history.push('/Products')}} style={{ cursor: 'pointer', color: '#a60b0b', fontWeight:'bold', marginTop:"12px" }}>
+							Click here to GO to Products Page.
+						</div>
+					</React.Fragment> :
+					<React.Fragment>
+					<div style={{ float: 'left', color: '#1d0707', fontWeight:'bold', marginBottom:"12px" }}>LOGIN</div>
+					<div>
+						<input type="text" value={this.state.userName} placeholder="Enter Username as admin" name="uname" onChange={this.changeuserName.bind(this)} required></input>
+					</div>
+					<div>
+						<input type="password" value={this.state.password} placeholder="Enter Password as admin" name="psw" onChange={this.changePassword.bind(this)}></input>
+					</div>
+					<input type="submit" name="submit" value="Continue" onClick={this.login}></input>
+					</React.Fragment>
+				}
                 </div>
+				</div>
             </div>
         )
     }
 }
 
-export default connect(null, null)(Login);
+const mapStateToProps=(state)=>{
+    return {reduxState : state};
+}
+
+export default connect(mapStateToProps, null)(Login);
